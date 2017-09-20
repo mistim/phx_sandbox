227b2294fe2254ca03643547d1fd86e98026b610
mistim <m.mistim@gmail.com>
2017-09-20 16:32:33 +0300

    - separate files of template; - use Materialize.Component for build navbar;

--- lib/sandbox_web/views/layout_view.ex
+++ lib/sandbox_web/views/layout_view.ex
@@ -1,3 +1,22 @@
 defmodule SandboxWeb.LayoutView do
   use SandboxWeb, :view
+  alias Materialize.Component, as: Materialize
+
+  def navbar(conn) do
+    Materialize.get_navbar([
+      [:options, mobile_collapse: true],
+      [:nav, class: "green lighten-2", role: "navigation"],
+      [:wrap, [class: "nav-wrapper container"]],
+      [:logo, "Matomato Club", class: "brand-logo"],
+      [
+        :ul,
+        [
+          [:a, "Статьи", [href: "#1"]],
+          [:a, "Видео", [href: "#2"]],
+          [:a, "Связь", [href: "#3"]]
+        ],
+        [id: "id-link"]
+      ]
+    ])
+  end
 end
